813d49b28d06f2792eeb0ef3c5bbfe7c84aeb567
deep.patel <deep.patel@nih.gov>
2019-08-16 09:05:15 -0400

    remove build packages

--- build/lib/swagger_server/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.index_schema_description import IndexSchemaDescription
-from swagger_server.models.index_search_attributes import IndexSearchAttributes
-from swagger_server.models.indexes import Indexes
-from swagger_server.models.search_attributes import SearchAttributes
